184a8b68f6cf61bbc90104d854902ae863093786
Hagos Alemseged <hagosalemeri@gmail.com>
2023-03-21 07:12:26 -0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,10 @@
-
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+// url for json data
+$url = 'https://jsonplaceholder.typicode.com/comments?postId=3';
+// put the contents of the json into a variable
+$data = file_get_contents($url); 
+// decode the JSON feed
+$items = json_decode($data);
+echo json_encode($items);
